9ec30aee851a44760aeb5ad57f43fcda4d5c683e
Amh <abaams78@gmail.com>
2020-05-25 22:03:56 -0500

    add dependency and annotation that is it

--- src/main/java/com/example/SpringSecurityFacebookLoginApplication.java
+++ src/main/java/com/example/SpringSecurityFacebookLoginApplication.java
@@ -1,9 +1,11 @@
-package com.example.springsecurityfacebooklogin;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringSecurityFacebookLoginApplication {
 
 	public static void main(String[] args) {
